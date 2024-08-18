586727912f3748b65ba006a6e482464414d219d5
barkevsarkli <141057220+barkevsarkli@users.noreply.github.com>
2024-08-18 15:57:48 +0300

    Add files via upload

---
+++ 00_I_Am_Poor_App/i_am_poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i_am_poor
+//
+//  Created by Barkev Şarklı on 11.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
